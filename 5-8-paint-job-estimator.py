54e1ec5b909843448b60d19c02e0a862b707b76e
Shaun Hayworth <thismoderndeath@gmail.com>
2019-11-14 21:53:27 -0800

    First draft of program code

---
+++ 5-8-paint-job-estimator.py
@@ -0,0 +1,60 @@
+
+# 5-8 Paint Job Estimator
+# Shaun Hayworth
+# CIS 2
+# 11-14-2019
+# Source code and revision history are available at
+# https://github.com/SCHayworth/5-8-Paint-Job-Estimator
+
+# Program calculates and displays labor and material costs for painting a given
+# space. Uses the cost_estimate function for calculations and printing the
+# results to the screen.
+
+# Initialize global constants for the number of feet per gallon of paint, the
+# hours of labor per one gallon of paint, and the labor cost per hour.
+FEET_PER_GALLON = 112
+LABOR_HOURS = 8
+LABOR_PER_HOUR = 35.0
+
+
+def main():
+    """ Executes the mainline program logic
+    """
+    # Prompt the user for the square footage of wall space and the price per
+    # gallon of paint.
+    wall_space = int(input('Enter wall space in sqare feet: '))
+    price_per_gallon = float(input('Enter paint price per gallon: '))
+
+    # Call the cost_estimate function and pass wall_space and price_per_gallon
+    # as arguments
+    cost_estimate(wall_space, price_per_gallon)
+
+
+def cost_estimate(feet, paint):
+    """ Calculates the amount of paint needed, the hours of labor needed, and the
+    total cost of labor and paint and prints the results to the screen.
+    """
+    # If feet parameter is evenly divisible by FEET_PER_GALLON, assign the
+    # quotient to paint_needed. Otherwise, add 1 to the quotient and assign it
+    # to paint_needed.
+    if feet % FEET_PER_GALLON == 0:
+        paint_needed = feet / FEET_PER_GALLON
+    else:
+        paint_needed = (feet / FEET_PER_GALLON) + 1
+
+    # Calculate the total hours and cost of labor, the total cost of paint, and
+    # the total estimated cost of the job.
+    labor_needed = paint_needed * LABOR_HOURS
+    paint_cost = paint_needed * paint
+    labor_cost = labor_needed * LABOR_PER_HOUR
+    total_estimate = paint_cost + labor_cost
+
+    # Print the results to the screen.
+    print(f'Gallons of paint: {paint_needed}'
+          f'Hours of labor: {labor_needed}'
+          f'Paint charges: ${paint_cost:,.2f}'
+          f'Labor charges: ${labor_cost:,.2f}'
+          f'Total cost: ${total_estimate:,.2f}')
+
+# Call the main() function to execute the program.
+main()
